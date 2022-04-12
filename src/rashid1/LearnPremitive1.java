d2acf084533f52b84841a331e576fa7d6fff74c8
Mahbubur Rashid <mahbuburrashidusa@gmail.com>
2022-04-11 23:25:03 -0400

    this is my sample message

---
+++ src/rashid1/LearnPremitive1.java
@@ -0,0 +1,64 @@
+package rashid1;
+
+public class LearnPremitive1 {
+	
+	
+//	
+//	
+//String instructorName ="Mahmud";	
+//public String address = "Queens,NY";	
+//String studentName	= "Rashid";
+//int age = 25	;
+//static int Salary = 6000 ;
+//	
+//	
+//public static void main(String[]args) {
+	
+	
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -128;
+		short salary = 3200;
+		
+		System.out.println(num); // calling a variable
+		
+		System.out.println ("Mahbub");
+		System.out.println(firstName); //calling a variable
+		System.out.println(lastName);  //calling a variable
+		
+		System.out.println(number);
+		
+		
+		System.out.println(salary);
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+
+	
+
---
+++ src/rashid2/LearnPremitive2.java
@@ -0,0 +1,56 @@
+package rashid2;
+
+public class LearnPremitive2 {
+	
+	
+	
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -127;
+		short salary = 5000;
+		
+		System.out.println(num); // calling a variable
+		
+		System.out.println ("Mahbub");
+		System.out.println(firstName); //calling a variable
+		System.out.println(lastName);  //calling a variable
+		
+		System.out.println(number);
+		
+		
+		System.out.println(salary);
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
